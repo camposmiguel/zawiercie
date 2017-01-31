efafd17d6b59e6aaa81c36aad8008f9a266cf293
Miguel Campos <camposmiguel@gmail.com>
2017-01-31 17:27:18 +0100

    Calc

--- 01_Basics/02_CalculatorLinearLayout/app/src/main/java/com/miguelcr/a01_helloworld/HelloWorldActivity.java
+++ 01_Basics/02_CalculatorLinearLayout/app/src/main/java/com/miguelcr/a01_helloworld/HelloWorldActivity.java
@@ -2,6 +2,7 @@ package com.miguelcr.a01_helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,9 +15,34 @@ public class HelloWorldActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
 
-        btn1 = (Button)findViewById(R.id.button1);
+        //btn1 = (Button)findViewById(R.id.button1);
         screen = (TextView) findViewById(R.id.textViewScreen);
 
-        screen.setText("HELLO");
+        screen.setText("0");
+    }
+
+    public void buttonClicked(View view) {
+        int id = view.getId();
+
+        // OPTION 1
+        /*
+        if(id==R.id.button1) {
+            screen.setText("1");
+        } else if(id==R.id.button2) {
+            screen.setText("2");
+        } ...
+        */
+
+        // OPTION 2
+        /*
+        switch (id) {
+            case R.id.button1: screen.setText("1"); break;
+            case R.id.button2: screen.setText("2"); break;
+            case R.id.button3: screen.setText("3"); break;
+        }
+        */
+
+        Button btnClicked = (Button) view;
+        screen.setText(btnClicked.getText());
     }
 }
