4acfe7203ad1e769899ccf9b0e8e02dcefe4ce13
Miguel Campos <camposmiguel@gmail.com>
2017-01-31 19:32:34 +0100

    RelativeLayout

--- 01_Basics/02_CalculatorLinearLayout/app/src/main/java/com/miguelcr/a01_helloworld/HelloWorldActivity.java
+++ 01_Basics/02_CalculatorLinearLayout/app/src/main/java/com/miguelcr/a01_helloworld/HelloWorldActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 public class HelloWorldActivity extends AppCompatActivity {
     Button btn1;
     TextView screen;
+    int operand1, operand2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,15 +35,43 @@ public class HelloWorldActivity extends AppCompatActivity {
         */
 
         // OPTION 2
-        /*
-        switch (id) {
-            case R.id.button1: screen.setText("1"); break;
-            case R.id.button2: screen.setText("2"); break;
-            case R.id.button3: screen.setText("3"); break;
-        }
-        */
 
-        Button btnClicked = (Button) view;
-        screen.setText(btnClicked.getText());
+        String screenText = "";
+        switch (id) {
+            case R.id.button1: screenText="1"; break;
+            case R.id.button2: screenText="2"; break;
+            case R.id.button3: screenText="3"; break;
+        }
+
+        String previousScreenText = screen.getText().toString(); // "12" > String
+
+
+        if(previousScreenText.equals("0")) {
+            screen.setText(screenText);
+        } else {
+            screen.setText(previousScreenText + screenText);
+        }
+
+
+        // OPTION 3
+        //Button btnClicked = (Button) view;
+        //screen.setText(btnClicked.getText());
+    }
+
+    public void addButtonClicked(View view) {
+        // Save the current number in the screen (1st operand)
+        String previousScreenText = screen.getText().toString(); // "12" > String
+        operand1 = Integer.parseInt(previousScreenText); // 12 > integer
+
+        // Clear the screen
+        screen.setText("0");
+    }
+
+    public void equalButtonClicked(View view) {
+        String previousScreenText = screen.getText().toString(); // "12" > String
+        operand2 = Integer.parseInt(previousScreenText); // 12 > integer
+
+        int result = operand1 + operand2;
+        screen.setText(String.valueOf(result));
     }
 }
